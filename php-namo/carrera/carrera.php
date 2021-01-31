a756c6f9af9283b0bb2c6cb9b732720234db6a53
educardenas97 <eduardocardenas97@gmail.com>
2021-01-31 12:35:36 -0300

    Commit

--- php-namo/carrera/carrera.php
+++ php-namo/carrera/carrera.php
@@ -3,16 +3,26 @@
     header("Content-Type: application/json; charset=UTF-8");
     require("carrera.class.php");
 
-    if($_SERVER['REQUEST_METHOD'] == 'GET'){
-        $carrera = new Carrera();
-        echo $carrera->getAllCarrera();
-    }elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
-        echo json_encode(Array('messaje' => 'POST METHOD'));
-    }elseif($_SERVER['REQUEST_METHOD'] == 'PUT'){
-        echo json_encode(Array('messaje' => 'PUT METHOD'));
-    }elseif($_SERVER['REQUEST_METHOD'] == 'DELETE'){
-        echo json_encode(Array('messaje' => 'DELETE METHOD'));
-    }else{
-        echo json_encode(Array('messaje' => 'NO VALID METHOD'));
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            try {
+                $carrera = new Carrera();
+                echo $carrera->getAllCarrera();
+            } catch (\Throwable $th) {
+                echo $th;
+            }
+            break;        
+        case 'POST':
+            try {
+                $carrera = new Carrera();
+                echo json_encode($carrera->setCarrera($_POST['detalle']));
+            } catch (\Throwable $th) {
+                echo $th;
+            }
+            break;
+        default:
+            echo "no valid method";
+            break;
     }
+
 ?>
